51c48e1192cba8e68f92f1f66fbfcd7e7c416659
aterwilliger <aterwilliger@spiria.com>
2019-05-23 11:57:10 -0400

    Completed Roman Numerals exercise.

--- Go/roman-numerals/roman_numerals.go
+++ Go/roman-numerals/roman_numerals.go
@@ -1,7 +1,13 @@
+// Package romannumerals computes Roman Numerals given a "regular" (aka digital) number
 package romannumerals
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
+// Numerals is a struct that holds the relevant representations in an order of magnitude -
+// the one, five, and ten characters for a single power of ten in Roman Numerals.
 type Numerals struct {
 	// Character equal to ONE unit
 	one string
@@ -11,6 +17,8 @@ type Numerals struct {
 	ten string
 }
 
+// Translator is a map of index type integer and value type Numerals. The index represents the
+// order of magnitude to which the Numerals belong.
 type Translator map[int]Numerals
 
 const one = "I"
@@ -21,7 +29,10 @@ const hundred = "C"
 const fivehundred = "D"
 const thousand = "M"
 
+// InitializeTranslator initializes and returns a translator object since I cannot have a constant map.
+// In this case it is initialized to support numbers up to 3000 only.
 func InitializeTranslator() (translator Translator) {
+	translator = make(map[int]Numerals)
 	translator[1] = InitializeNumerals(one, five, ten)
 	translator[2] = InitializeNumerals(ten, fifty, hundred)
 	translator[3] = InitializeNumerals(hundred, fivehundred, thousand)
@@ -30,6 +41,7 @@ func InitializeTranslator() (translator Translator) {
 	return
 }
 
+// InitializeNumerals initializes and returns a Numerals struct for one power of ten
 func InitializeNumerals(one string, five string, ten string) (numerals Numerals) {
 	numerals = Numerals{
 		one:  one,
@@ -40,18 +52,48 @@ func InitializeNumerals(one string, five string, ten string) (numerals Numerals)
 	return
 }
 
-func ToRomanNumeral(number int) (numeral string) {
+// ToRomanNumeral takes a digital number and translates it to its Roman Numeral equivalent.
+func ToRomanNumeral(number int) (numeral string, err error) {
+	if number <= 0 || number > 3000 {
+		return "", errors.New("can only translate numbers between 1 and 3000, inclusive")
+	}
+
 	var translator = InitializeTranslator()
 	var slice = strconv.Itoa(number)
 
-	numeral += DigitToRomanNumeral(slice[0], len(slice)-0, translator)
-	numeral += DigitToRomanNumeral(slice[1], len(slice)-1, translator)
-	numeral += DigitToRomanNumeral(slice[2], len(slice)-2, translator)
-	numeral += DigitToRomanNumeral(slice[3], len(slice)-3, translator)
+	for pos, char := range slice {
+		numeral += DigitToRomanNumeral(char, len(slice)-pos, translator)
+	}
+
 	return
 }
 
-func DigitToRomanNumeral(digit byte, orderOfMagnitude int, translator Translator) (numeral string) {
+// DigitToRomanNumeral translates a single digit to its Roman Numeral equivalent.
+func DigitToRomanNumeral(digit rune, orderOfMagnitude int, translator Translator) (numeral string) {
+	var numerals = translator[orderOfMagnitude]
+
+	switch digit {
+	case '1':
+		numeral = numerals.one
+	case '2':
+		numeral = numerals.one + numerals.one
+	case '3':
+		numeral = numerals.one + numerals.one + numerals.one
+	case '4':
+		numeral = numerals.one + numerals.five
+	case '5':
+		numeral = numerals.five
+	case '6':
+		numeral = numerals.five + numerals.one
+	case '7':
+		numeral = numerals.five + numerals.one + numerals.one
+	case '8':
+		numeral = numerals.five + numerals.one + numerals.one + numerals.one
+	case '9':
+		numeral = numerals.one + numerals.ten
+	default:
+		numeral = ""
+	}
 
 	return
 }
